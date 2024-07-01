94db788cb5ff681ec1462922fc4b81f6360fa067
ganxiaohua <543123001@qq.com>
2024-07-01 11:28:28 +0800

    opt code

--- Scripts/Runtime/Process/GameInitState.cs
+++ Scripts/Runtime/Process/GameInitState.cs
@@ -7,7 +7,8 @@ namespace GXGame
         public override void Enter(FsmController fsmController)
         {
             base.Enter(fsmController);
-            Config.Instance.LoadTable();
+            // Config.Instance.LoadTable();
+            //播放一个icon之类
             fsmController.SwitchState<GameStartState>();
         }
     }
--- Scripts/Runtime/Scene/CubeScene.cs
+++ Scripts/Runtime/Scene/CubeScene.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using GameFrame;
 
