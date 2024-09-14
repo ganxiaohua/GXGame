d14b4cc34d5d0694d094f866bfed3bf0101883dd
ganxiaohua <543123001@qq.com>
2024-09-14 10:05:29 +0800

    skinRenderer

--- Temp/BatchRenderGroup/SkinRenderer.cs
+++ Temp/BatchRenderGroup/SkinRenderer.cs
@@ -1,7 +1,10 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using Sirenix.OdinInspector;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Profiling;
@@ -40,22 +43,23 @@ public class SkinRenderer : MonoBehaviour
     private Transform rootBones;
     private Vector3[] vertices;
     private BoneWeight[] boneWeights;
-    private Vector3[] newVertices;
     private Matrix4x4[] bindPose;
     private Matrix4x4[] skinMartixs;
     private float[] trsData;
     public SkinQuality skinQuailty = SkinQuality.Bone1;
     private Data[] bonesData;
+    private NativeArray<BoneWeight> boneWeightsNative;
+    private NativeArray<Vector3> verticesNativeArray;
+    private NativeArray<Vector3> outVerticesArray;
 
     void Start()
     {
         mesh = Object.Instantiate(renderers.sharedMesh);
         mesh.MarkDynamic();
         meshFilter.mesh = mesh;
-        mesh.name = "SkinnedMesh";
+        mesh.name = "Mesh";
         vertices = mesh.vertices;
         bindPose = mesh.bindposes;
-        newVertices = new Vector3[vertices.Length];
         boneWeights = mesh.boneWeights;
         mat = renderers.sharedMaterial;
         bones = renderers.bones;
@@ -63,6 +67,9 @@ public class SkinRenderer : MonoBehaviour
         rootBones = renderers.rootBone ? renderers.rootBone : transform;
         bonesData = new Data[bones.Length];
         trsData = new float[(int) Types.Count];
+        boneWeightsNative = new NativeArray<BoneWeight>(boneWeights, Allocator.Persistent);
+        verticesNativeArray = new NativeArray<Vector3>(vertices, Allocator.Persistent);
+        outVerticesArray = new NativeArray<Vector3>(vertices.Length, Allocator.Persistent);
         Prepare();
     }
 
@@ -200,48 +207,48 @@ public class SkinRenderer : MonoBehaviour
                 float value = bonesdata.curve[j].Evaluate(time);
                 trsData[j] = value;
             }
+
             bone.localPosition = new Vector3(trsData[0], trsData[1], trsData[2]);
             bone.localRotation = new Quaternion(trsData[3], trsData[4], trsData[5], trsData[6]);
             bone.localScale = new Vector3(trsData[7], trsData[8], trsData[9]);
         }
-
         for (int i = 0; i < bones.Length; i++)
         {
             skinMartixs[i] = GetSkinMatrix(ref worldToRoot, bones, i);
         }
 
-        //设置蒙皮矩阵
-        Profiler.BeginSample("changge outVerticesArray");
-        int vCount = vertices.Length;
-        Matrix4x4 blendSkin = Matrix4x4.identity;
-        for (int i = 0; i < vCount; i++)
+        NativeArray<Matrix4x4> skinMartix0Native = new NativeArray<Matrix4x4>(vertices.Length, Allocator.TempJob);
+        NativeArray<Matrix4x4> skinMartix1Native = new NativeArray<Matrix4x4>(vertices.Length, Allocator.TempJob);
+        NativeArray<Matrix4x4> skinMartix2Native = new NativeArray<Matrix4x4>(vertices.Length, Allocator.TempJob);
+        NativeArray<Matrix4x4> skinMartix3Native = new NativeArray<Matrix4x4>(vertices.Length, Allocator.TempJob);
+
+        for (int i = 0; i < vertices.Length; i++)
         {
-            if (skinQuailty == SkinQuality.Bone1)
-            {
-                int boneIndex = boneWeights[i].boneIndex0;
-                blendSkin = skinMartixs[boneIndex];
-            }
-            else if (skinQuailty == SkinQuality.Bone2)
-            {
-                ref BoneWeight bw = ref boneWeights[i];
-                //2根骨骼权重混合
-                blendSkin = MatrixWeight(skinMartixs[bw.boneIndex0], bw.weight0);
-                blendSkin = MatrixAdd(blendSkin, MatrixWeight(skinMartixs[bw.boneIndex1], bw.weight1));
-            }
-            else if (skinQuailty == SkinQuality.Bone4)
-            {
-                ref BoneWeight bw = ref boneWeights[i];
-                //4根骨骼权重混合
-                blendSkin = MatrixWeight(skinMartixs[bw.boneIndex0], bw.weight0);
-                blendSkin = MatrixAdd(blendSkin, MatrixWeight(skinMartixs[bw.boneIndex1], bw.weight1));
-                blendSkin = MatrixAdd(blendSkin, MatrixWeight(skinMartixs[bw.boneIndex2], bw.weight2));
-                blendSkin = MatrixAdd(blendSkin, MatrixWeight(skinMartixs[bw.boneIndex3], bw.weight3));
-            }
-            newVertices[i] = blendSkin.MultiplyPoint3x4(vertices[i]);
+            skinMartix0Native[i] = skinMartixs[boneWeights[i].boneIndex0];
+            skinMartix1Native[i] = skinMartixs[boneWeights[i].boneIndex1];
+            skinMartix2Native[i] = skinMartixs[boneWeights[i].boneIndex2];
+            skinMartix3Native[i] = skinMartixs[boneWeights[i].boneIndex3];
         }
-        Profiler.EndSample();
-        mesh.vertices = newVertices;
+        
+        var moveJob = new VertexBlend
+        {
+            SkinMatrix0 = skinMartix0Native,
+            SkinMatrix1 = skinMartix1Native,
+            SkinMatrix2 = skinMartix2Native,
+            SkinMatrix3 = skinMartix3Native,
+            boneWeight = boneWeightsNative,
+            inVertices = verticesNativeArray,
+            outVertices = outVerticesArray,
+        };
+        
+        var sheduleJobHandle = moveJob.Schedule(vertices.Length,128);
+        sheduleJobHandle.Complete();
+        mesh.vertices = outVerticesArray.ToArray();
         mesh.UploadMeshData(false);
+        skinMartix0Native.Dispose();
+        skinMartix1Native.Dispose();
+        skinMartix2Native.Dispose();
+        skinMartix3Native.Dispose();
     }
 
 
@@ -259,47 +266,72 @@ public class SkinRenderer : MonoBehaviour
         return worldToRoot * bone.localToWorldMatrix * bindPose[index];
     }
 
-    //矩阵乘float
-    Matrix4x4 MatrixWeight(Matrix4x4 matrix4X4, float weight)
+    struct VertexBlend : IJobParallelFor
     {
-        matrix4X4.m00 *= weight;
-        matrix4X4.m01 *= weight;
-        matrix4X4.m02 *= weight;
-        matrix4X4.m03 *= weight;
-        matrix4X4.m10 *= weight;
-        matrix4X4.m11 *= weight;
-        matrix4X4.m12 *= weight;
-        matrix4X4.m13 *= weight;
-        matrix4X4.m20 *= weight;
-        matrix4X4.m21 *= weight;
-        matrix4X4.m22 *= weight;
-        matrix4X4.m23 *= weight;
-        matrix4X4.m30 *= weight;
-        matrix4X4.m31 *= weight;
-        matrix4X4.m32 *= weight;
-        matrix4X4.m33 *= weight;
-        return matrix4X4;
-    }
+        public NativeArray<Matrix4x4> SkinMatrix0;
+        public NativeArray<Matrix4x4> SkinMatrix1;
+        public NativeArray<Matrix4x4> SkinMatrix2;
+        public NativeArray<Matrix4x4> SkinMatrix3;
+        public NativeArray<BoneWeight> boneWeight;
 
-    //矩阵相加
-    Matrix4x4 MatrixAdd(Matrix4x4 a, Matrix4x4 b)
-    {
-        a.m00 += b.m00;
-        a.m01 += b.m01;
-        a.m02 += b.m02;
-        a.m03 += b.m03;
-        a.m10 += b.m10;
-        a.m11 += b.m11;
-        a.m12 += b.m12;
-        a.m13 += b.m13;
-        a.m20 += b.m20;
-        a.m21 += b.m21;
-        a.m22 += b.m22;
-        a.m23 += b.m23;
-        a.m30 += b.m30;
-        a.m31 += b.m31;
-        a.m32 += b.m32;
-        a.m33 += b.m33;
-        return a;
+        public NativeArray<Vector3> inVertices;
+        public NativeArray<Vector3> outVertices;
+
+        [BurstCompile]
+        public void Execute(int index)
+        {
+            Matrix4x4 blendSkin = Matrix4x4.identity;
+            BoneWeight bw = boneWeight[index];
+            //4根骨骼权重混合
+            blendSkin = MatrixWeight(SkinMatrix0[index], bw.weight0);
+            blendSkin = MatrixAdd(blendSkin, MatrixWeight(SkinMatrix1[index], bw.weight1));
+            blendSkin = MatrixAdd(blendSkin, MatrixWeight(SkinMatrix2[index], bw.weight2));
+            blendSkin = MatrixAdd(blendSkin, MatrixWeight(SkinMatrix3[index], bw.weight3));
+            outVertices[index] = blendSkin.MultiplyPoint3x4(inVertices[index]);
+        }
+
+        //矩阵乘float
+        Matrix4x4 MatrixWeight(Matrix4x4 matrix4X4, float weight)
+        {
+            matrix4X4.m00 *= weight;
+            matrix4X4.m01 *= weight;
+            matrix4X4.m02 *= weight;
+            matrix4X4.m03 *= weight;
+            matrix4X4.m10 *= weight;
+            matrix4X4.m11 *= weight;
+            matrix4X4.m12 *= weight;
+            matrix4X4.m13 *= weight;
+            matrix4X4.m20 *= weight;
+            matrix4X4.m21 *= weight;
+            matrix4X4.m22 *= weight;
+            matrix4X4.m23 *= weight;
+            matrix4X4.m30 *= weight;
+            matrix4X4.m31 *= weight;
+            matrix4X4.m32 *= weight;
+            matrix4X4.m33 *= weight;
+            return matrix4X4;
+        }
+
+        //矩阵相加
+        Matrix4x4 MatrixAdd(Matrix4x4 a, Matrix4x4 b)
+        {
+            a.m00 += b.m00;
+            a.m01 += b.m01;
+            a.m02 += b.m02;
+            a.m03 += b.m03;
+            a.m10 += b.m10;
+            a.m11 += b.m11;
+            a.m12 += b.m12;
+            a.m13 += b.m13;
+            a.m20 += b.m20;
+            a.m21 += b.m21;
+            a.m22 += b.m22;
+            a.m23 += b.m23;
+            a.m30 += b.m30;
+            a.m31 += b.m31;
+            a.m32 += b.m32;
+            a.m33 += b.m33;
+            return a;
+        }
     }
 }
